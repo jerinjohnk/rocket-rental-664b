e634eeed41cda2e25f71187177e700130bca91aa
Jerin John K <jerin@policybazaar.com>
2022-12-14 12:52:18 +0530

    reverted version upgrades

--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -3,17 +3,14 @@ import invariant from 'tiny-invariant'
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-            DATABASE_URL: string
-            SESSION_SECRET: string
+			DATABASE_URL: string
+			SESSION_SECRET: string
 		}
 	}
 }
 
 export function init() {
-	const requiredEnvs = [
-		'DATABASE_URL',
-		'SESSION_SECRET',
-	] as const
+	const requiredEnvs = ['DATABASE_URL', 'SESSION_SECRET'] as const
 	for (const env of requiredEnvs) {
 		invariant(process.env[env], `${env} is required`)
 	}
